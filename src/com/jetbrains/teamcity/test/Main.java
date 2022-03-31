8f4589a694c9593d18c1a405d2d0e27096063b97
Nadia Burnasheva <nadezhda.burnasheva@jetbrains.com>
2022-03-31 17:18:53 +0200

    first commit

---
+++ src/com/jetbrains/teamcity/test/Main.java
@@ -0,0 +1,10 @@
+package com.jetbrains.teamcity.test;
+
+import java.io.Console;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
